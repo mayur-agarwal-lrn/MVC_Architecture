3d00d5a4ff32e8762467e07eabfb3c367691b272
Mayur Agarwal <mayuragarwal7@outlook.com>
2019-07-28 09:49:44 +0530

    Data Annotations

--- MVCArchitecture.Web/Models/Student.cs
+++ MVCArchitecture.Web/Models/Student.cs
@@ -8,10 +8,13 @@ namespace UniversityApp.Models
     public class Student
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required, Display(Name = "First Name")]
         public string FirstName { get; set; }
-        [Required, MaxLength(30)]
+
+        [Required, MaxLength(30), Display(Name = "Last Name")]
         public string LastName { get; set; }
+
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
